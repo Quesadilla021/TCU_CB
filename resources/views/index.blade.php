569c29b44c4f89bc9ad462909896143a637a023d
Quesadilla021 <ianques021@gmail.com>
2023-09-20 20:14:29 -0600

    Actualizacion inicio

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -29,8 +29,8 @@
                     <ul class="navbar-nav text-uppercase ms-auto py-4 py-lg-0">
                         <li class="nav-item"><a class="nav-link" href="#services">Servicios</a></li>
                         <li class="nav-item"><a class="nav-link" href="#portfolio">Portfolio</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#about">About</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#team">Team</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#about">Sobre nosotros</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#team">Equipo</a></li>
                         <li class="nav-item"><a class="nav-link" href="#contact">Contactos</a></li>
                     </ul>
                 </div>
