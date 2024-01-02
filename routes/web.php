c70163fe79919f381b1bb3a19b09a5986dd81bfe
Quesadilla021 <ianques021@gmail.com>
2024-01-02 17:27:57 -0600

    Admin Template CB

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('/Publicitaria/Landing/index');
 });
+
+Route::get('/admin', function () {
+    return view('/Publicitaria/Administrativa/index');
+})->name('inicioAdmin');
